c8338713b2c5777eec7cbb6a62e138dfcb0c7ff1
mohand mostafa <mohand.mostafa9811@gmail.com>
2022-04-26 04:46:44 +0200

    match the design by make chart line have multiple ranage every range have spesifc color and match tooltip by provide the point's date as well as the relative performance index colour and value

--- app/components/vue-components/performance-chart.vue
+++ app/components/vue-components/performance-chart.vue
@@ -36,8 +36,8 @@ export default {
   },
   data() {
     return {
-      startDate:null,
-      endDate:null,
+      startDate: null,
+      endDate: null,
     };
   },
   created() {
@@ -48,7 +48,7 @@ export default {
 
   computed: {
     originChartData() {
-     return this.$store.getters["performance/getOriginChartData"];
+      return this.$store.getters["performance/getOriginChartData"];
     },
     chartData: {
       get() {
@@ -77,6 +77,16 @@ export default {
           confine: false,
           hideDelay: 0,
           padding: 0,
+          formatter: function (params) {
+            return `
+        <div class="c-chart__tooltip">
+          <div class="c-chart__tooltip-title">${params[0].axisValueLabel}</div>
+          <div class="c-chart__tooltip-content">
+                ${params[0].marker} Team Performance Index: ${params[0].data}%
+          </div>
+        </div>
+`;
+          },
         },
         grid: {
           left: "30px",
@@ -116,6 +126,27 @@ export default {
             },
           },
         ],
+        visualMap: {
+          top: 50,
+          right: 10,
+          pieces: [
+            {
+              gt: 0,
+              lte: 50,
+              color: "red",
+            },
+            {
+              gt: 50,
+              lte: 80,
+              color: "yellow",
+            },
+            {
+              gt: 80,
+              lte: 100,
+              color: "green",
+            },
+          ],
+        },
       };
     },
 
@@ -132,20 +163,51 @@ export default {
     formatDate(dateInMs) {
       return moment(dateInMs).format("DD MMM YYYY");
     },
-    fiterChartDataByDate(startDate, endDate){  
-      const formateDateToInputFormat = (date) => moment(date).format("YYYY-MM-DD")
-      const filterChartData = this.originChartData.filter(({date_ms}) => moment(formateDateToInputFormat(date_ms)).isBetween(startDate, endDate, undefined, '[]'));
-      if(filterChartData.length) this.chartData = filterChartData
-      else this.chartData = this.originChartData
-    }
+    fiterChartDataByDate(startDate, endDate) {
+      const formateDateToInputFormat = (date) =>
+        moment(date).format("YYYY-MM-DD");
+      const filterChartData = this.originChartData.filter(({ date_ms }) =>
+        moment(formateDateToInputFormat(date_ms)).isBetween(
+          startDate,
+          endDate,
+          undefined,
+          "[]"
+        )
+      );
+      if (filterChartData.length) this.chartData = filterChartData;
+      else this.chartData = this.originChartData;
+    },
   },
-  watch:{
-    startDate(startDate){
+  watch: {
+    startDate(startDate) {
       this.fiterChartDataByDate(startDate, this.endDate);
     },
-    endDate(endDate){
+    endDate(endDate) {
       this.fiterChartDataByDate(this.startDate, endDate);
-    }
-  }
+    },
+  },
 };
 </script>
+<style lang="scss" >
+.c-chart__container {
+  .c-chart__tooltip {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #223048;
+    border-radius: 5px;
+    padding: 10px;
+    .c-chart__tooltip-title {
+      font-size: 13px;
+      font-weight: bold;
+      color: #fff;
+    }
+    .c-chart__tooltip-content {
+      font-size: 14px;
+      color: #fff;
+    }
+    border: 1px solid #fff;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+  }
+}
+</style>
